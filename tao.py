5996dbcbbabcbf45c9ba98c53eb06bbc3e9c04a2
taofengno1 <taofeng_ls@sina.com>
2016-05-19 22:20:11 +0800

    完成定时发送

--- tao.py
+++ tao.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.scheduler import Scheduler
 import itchat, time
 
 itchat.auto_login()
+
+sched = Scheduler()
+sched.start()
 
 def task():
   chatroomList = itchat.get_chatrooms(False);
   for m in chatroomList:
     NickName = m['NickName'].encode('utf-8')
     if NickName == u'测试'.encode('utf-8'):
-      text = u'中文群发测试'.encode('utf-8')
+      text = u'可以定时发送了'.encode('utf-8')
       itchat.send(text, m['UserName'])
 
-sched = BlockingScheduler()
-sched.add_job(task, 'cron', month='1-12', day='1-31', hour=14, minute=32)
-sched.start()
+sched.add_cron_job(task, month='1-12', day='1-31', hour=21, minute=5)
+
+itchat.run()
